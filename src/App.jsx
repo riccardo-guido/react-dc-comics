cf6fcf8930b1349a67f157c1523dcf05e145fcf8
riccardo-guido <guidoriccardo02@gmail.com>
2025-05-20 19:31:47 +0200

    middlepagefooter-and-footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Middlepagefooter from "./components/Middlepagefooter";
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
+      <Middlepagefooter />
       <Footer />
     </>
   );
---
+++ src/components/Middlepagefooter.jsx
@@ -0,0 +1,19 @@
+export default function Middlepagefooter() {
+    return <div id="middlepagefooter">
+        <div id="middlepagefooter-container" className="container-xl">
+            
+              
+                <img src="/buy-comics-digital-comics.png" alt="DIGITAL COMICS" />
+            
+                <img src="/buy-comics-merchandise.png" alt="MERCHANDISE" />
+                <img src="/buy-comics-subscriptions.png" alt="SUBSCRIPTIONS" />
+                <img src="/buy-comics-shop-locator.png" alt="SHOP LOCATOR" />
+                <img src="/buy-dc-power-visa.svg" alt="DC POWER VISA" />
+
+               
+
+            
+        </div>
+
+    </div>
+}
