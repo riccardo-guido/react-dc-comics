ad0691457900c76d2b3f5814cdd87c77ae0e2ff3
riccardo-guido <guidoriccardo02@gmail.com>
2025-05-20 17:14:39 +0200

    default-structure-created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+    return <footer>Qui va il footer </footer>;
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,3 @@
+export default function Main() {
+    return <header>Qui va il Main </header>;
+}
